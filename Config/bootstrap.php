4c976a811f181125e0f852f3f1f078b3867cbc82
BrySi_ <bryansimon03@gmail.com>
2015-09-19 22:10:20 +0200

    Création du plugin

---
+++ Config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Bryan
+ * Date: 19/09/2015
+ * Time: 22:00
+ */
---
+++ Controller/NewsController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Bryan
+ * Date: 19/09/2015
+ * Time: 22:03
+ */
+class NewsController extends Last_NewsAppController {
+    public function last_news() {
+        $this->loadModel('News');
+        $last_news = $this->News->find('first', array('order' => 'DESC id'))['News'];
+    }
+}
