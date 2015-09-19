2a0028b8a5cb12b3dbb43fc93aa448fbf5f11f6b
BrySi_ <bryansimon03@gmail.com>
2015-09-19 22:34:27 +0200

    Changement du extends

--- Controller/LastnewsController.php
+++ Controller/LastnewsController.php
@@ -5,7 +5,7 @@
  * Date: 19/09/2015
  * Time: 22:03
  */
-class LastnewsController extends LastnewsAppController {
+class LastnewsController extends AppController {
     public function last_news() {
         $this->loadModel('News');
         $last_news = $this->News->find('first', array('order' => 'DESC id'))['News'];
