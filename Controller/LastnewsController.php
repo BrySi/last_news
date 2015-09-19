34492824d85a587a5ba8007da940c2c10f52e1dd
BrySi_ <bryansimon03@gmail.com>
2015-09-19 22:36:40 +0200

    On va y arriver

--- Controller/LastnewsController.php
+++ Controller/LastnewsController.php
@@ -9,5 +9,6 @@ class LastnewsController extends AppController {
     public function index() {
         $this->loadModel('News');
         $last_news = $this->News->find('first', array('order' => 'DESC id'))['News'];
+        $this->autoRender = false;
     }
 }
