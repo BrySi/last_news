ec50fd8dfb7fe98674d7593a4b898bac4d7773b9
BrySi_ <bryansimon03@gmail.com>
2015-09-20 00:46:01 +0200

    Giving a style

--- Controller/LastnewsController.php
+++ Controller/LastnewsController.php
@@ -9,5 +9,6 @@ class LastnewsController extends AppController {
     public function index() {
         $this->loadModel('News');
         $last_news = $this->News->find('first', array('order' => 'id DESC', 'conditions' => array('published' => 1)))['News'];
+        $this->set(compact('last_news'));
     }
 }
--- View/LastNews/index.ctp
+++ View/LastNews/index.ctp
@@ -1,1 +1,3 @@
-<h1>Hello</h1>
+<div class="container">
+    <h1><?= $last_news->title; ?></h1>
+</div>
